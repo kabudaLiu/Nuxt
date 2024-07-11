ee4a3cf825d61a3b2496cdb94e42a484135f9056
刘增杰 <lzj@lingzifu.com>
2024-07-11 21:42:29 +0800

    增加tailwindCSS

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <NuxtPage></NuxtPage>
 </template>
+<script setup lang="ts">
+useSeoMeta({
+    title: "我的神奇网站",
+    ogTitle: "我的神奇网站1",
+    description: "这是我的神奇网站，让我来告诉你关于它的一切。",
+    ogDescription: "这是我的神奇网站，让我来告诉你关于它的一切。",
+    ogImage: "https://example.com/image.png",
+    twitterCard: "summary_large_image",
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr:false
+  ssr:true,
+//   modules: ['@nuxtjs/tailwindcss'],
+css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
